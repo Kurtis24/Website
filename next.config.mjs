a25fd9b57c7ecb7388b54ab135bd95505b0e7b4b
KurtisL7 <kurtislin24@gmail.com>
2026-05-10 18:06:25 -0400

    disable img element warnings

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
       { protocol: 'https', hostname: 'cdn.pixabay.com' },
     ],
   },
+  eslint: {
+    // Disable the img element warning
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
